978a6259ce1c40d381f996e28ffd34f8a6c3c24c
kernelpanic007 <josalamanca007@gmail.com>
2024-04-05 16:37:10 -0500

    Update zabbixBC250Fix.sh

--- zabbixBC250Fix.sh
+++ zabbixBC250Fix.sh
@@ -8,7 +8,7 @@
 wget https://raw.githubusercontent.com/kernelpanic0007/hiveOSScripts/main/zabbix_agentd.conf -O /etc/zabbix/zabbix_agentd.conf
 
 #replace zabbix HostName with systems
-sed -i 's/zabbixipaddressvar/192.168.254.25/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/zabbixipaddressvar/10.0.0.149/g' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/hostnamevar/'"$HOSTNAME"'/g' /etc/zabbix/zabbix_agentd.conf
 
 #restart zabbix agent
